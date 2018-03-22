8440c019eac4c5436c86c1b6c5c7a2407ab8bfd4
Thiago Melo <thiago.lc.melo@gmail.com>
2018-03-22 12:10:15 -0300

    vai saber

---
+++ asymmetric_double_well.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+The asymmetric double well was studied by Feit, Fleck, and Steiger
+in 1982 as an application of the pseudo spectral method.
+
+My pleasure:
+
+@article{feit1982,
+  author  = {FEIT, M. D. and FLECK, J. A. and STEIGER, A.}, 
+  title   = {Solution of the Schr\"odinger Equation by a Spectral Method},
+  journal = {Journal of Computational Physics},
+  year    = 1982,
+  month   = 2,
+  pages   = {412-433},
+  volume  = 47
+}
+
+@ARTICLE{zhirnov1979,
+   author = {{ZHIRNOV}, N.~I. and {TUREV}, A.~V.},
+    title = "{Evaluation of transition probabilities in a double asymmetric potential well}",
+  journal = {Optics and Spectroscopy},
+     year = 1979,
+    month = nov,
+   volume = 47,
+    pages = {480-484},
+   adsurl = {http://adsabs.harvard.edu/abs/1979OptSp..47..480Z},
+  adsnote = {Provided by the SAO/NASA Astrophysics Data System}
+}
+"""
+
